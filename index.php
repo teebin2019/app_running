1b719e4b3f34c9a7a6d2575efbf5c974d581337b
teebin2019 <teebin2019@gmail.com>
2025-10-30 22:20:59 +0700

    update register

--- index.php
+++ index.php
@@ -12,37 +12,84 @@
 <body>
     <div class="container">
         <h1>ระบบสมัครวิ่ง</h1>
-        <form>
+        <form action="register_db.php" method="post">
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">ชื่อจริง</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">นามสกุล</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="f_name" class="form-label">ชื่อจริง <span class="text-danger">*</span></label>
+                <input type="text" class="form-control" id="f_name" name="f_name" required>
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">บัตรประชาชน</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="l_name" class="form-label">นามสกุล <span class="text-danger">*</span></label>
+                <input type="text" class="form-control" id="l_name" name="l_name" required>
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">อายุ</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="id_card" class="form-label">บัตรประชาชน <span class="text-danger">*</span></label>
+                <input type="text" class="form-control" id="id_card" name="id_card" required minlength="13" maxlength="13">
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">เพศ</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="age" class="form-label">อายุ <span class="text-danger">*</span></label>
+                <input type="number" class="form-control" id="age" name="age" required>
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">วันเกิด</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="gender" class="form-label">เพศ <span class="text-danger">*</span></label>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="gender1" value="ชาย">
+                    <label class="form-check-label" for="gender1">ชาย</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="gender2" value="หญิง">
+                    <label class="form-check-label" for="gender2">หญิง</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="gender3" value="ไม่ระบุ">
+                    <label class="form-check-label" for="gender3">ไม่ระบุ</label>
+                </div>
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <div class="mb-3">
+                <label for="birth_day" class="form-label">วันเกิด <span class="text-danger">*</span></label>
+                <input type="date" class="form-control" id="birth_day" name="birth_day" required>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+
+            <h5>ประเภทการวิ่ง</h5>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ประเภท</th>
+                        <th scope="col">ราคา</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">วิ่งเดี่ยว</th>
+                        <td>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" id="price1" name="price[]" value="100">
+                                <label class="form-check-label" for="price1">100 บาท</label>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">วิ่งคู่</th>
+                        <td>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" id="price2" name="price[]" value="150">
+                                <label class="form-check-label" for="price2">150 บาท</label>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">วิ่งหลายคน</th>
+                        <td>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" id="price3" name="price[]" value="200">
+                                <label class="form-check-label" for="price3">200 บาท</label>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <button type="submit" class="btn btn-primary">ยืนยัน</button>
         </form>
 
     </div>
--- register_db.php
+++ register_db.php
@@ -1,7 +1,7 @@
 <?php
 
+include_once 'connection.php';
 try {
-    include_once 'connection.php';
 
     // insert a row
     $id_card = $_POST['id_card'];
@@ -10,6 +10,7 @@ try {
     $age = $_POST['age'];
     $gender = $_POST['gender'];
     $birth_day = $_POST['birth_day'];
+    $prices = $_POST['price']; // เก็บเป็น array
 
     // prepare sql and bind parameters
     $stmt = $conn->prepare("INSERT INTO users (id_card,	f_name,	l_name,	age,	gender	,birth_day	)
@@ -23,7 +24,60 @@ try {
 
     $stmt->execute();
 
-    echo "New records created successfully";
+    foreach ($prices as $price) {
+        switch ($price) {
+            case 100:
+                $type = "วิ่งเดี่ยว";
+                break;
+            case 150:
+                $type = "วิ่งคู่";
+                break;
+            case 200:
+                $type = "วิ่งหลายคน";
+                break;
+            default:
+                $type = "ไม่ระบุประเภทการวิ่ง";
+        }
+
+        // prepare sql and bind parameters
+        $stmt = $conn->prepare("INSERT INTO running_type (type ,id_card,	price	)
+  VALUES (:type, :id_card ,  :price)");
+        $stmt->bindParam(':type', $type);
+        $stmt->bindParam(':id_card', $id_card);
+        $stmt->bindParam(':price', $price);
+
+        $result = $stmt->execute();
+    }
+
+    // sweet alert 
+    echo '
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">';
+
+    if ($result) {
+        echo '<script>
+             setTimeout(function() {
+              swal({
+                  title: "เพิ่มข้อมูลสำเร็จ",
+                  type: "success"
+              }, function() {
+                  window.location = "index.php"; //หน้าที่ต้องการให้กระโดดไป
+              });
+            }, 1000);
+        </script>';
+    } else {
+        echo '<script>
+             setTimeout(function() {
+              swal({
+                  title: "เกิดข้อผิดพลาด",
+                  type: "error"
+              }, function() {
+                  window.location = "index.php"; //หน้าที่ต้องการให้กระโดดไป
+              });
+            }, 1000);
+        </script>';
+    }
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
